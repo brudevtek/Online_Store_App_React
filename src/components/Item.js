f674162b78d5714a558a3a2607c8f5c45f4d2504
dev_only <dev_only@Bs-MacBook-Pro.local>
2023-01-19 07:18:55 -0600

    Increasing count is working and gets properly stored locally

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,41 +1,101 @@
-import React, { useState} from 'react';
+
+import React, { Component } from 'react';
 
 
+//  const bucket= JSON.parse(localStorage.getItem("data")) || 0;
 
+const dataHub = [{}]
 
-const Item = (props) =>{
-  
-  
-     return(
+class Item extends Component{
+    constructor() {
+        super();
+        // this.state = JSON.parse(window.localStorage.getItem('state')) || { 
+        //     counter: 0 , 
+        //     id:0};
+      this.state = { 
+        counter: 0 , 
+        id:0};
+            
+      }
       
-       <div className="item">
-         <div className="itemcard_up">
-             <img src={props.image} alt="item" />
-             <h1>{props.name}</h1>
-             <p>{props.desc}</p>
-           <div className="itemcard_down">
-               <div className="total_price">
-                   <h2>$ {props.price}</h2>  
-               </div>
-               <div className="item_icons">
-                   <div>
-                       <i>-</i>  
-                   </div>
-                   <div className="one_item_counter">
-                       <h1>0</h1>
-                   </div>
-                   <div>
-                       <i>+</i>
-                    </div>
-               </div>
-           </div>
-         </div>
-     </div>
+      componentDidMount() {  
+        // window.localStorage.setItem('state', JSON.stringify(this.state));
+        // const dataHub=[{}]
+        
+            
+      }
 
- 
-     );
-     
+    //   setState(state) {
+    //     window.localStorage.setItem('state', JSON.stringify(state));
+    //     super.setState(state);
+    //   }
 
-   }
+    
+
+      IncreaseByOne = (id) => {
+        
+        this.setState({counter: this.state.counter + 1,id:this.props.id })
+        
+      }
+      DecreaseByOne = (id) => {
+        if (this.state.counter ===0){
+            // do nothing
+        } else {
+            this.setState({counter: this.state.counter - 1,id:this.props.id })
+            }
+            
+          
+            
+
+        }
+        
+      
+      componentDidUpdate() {
+        // localStorage.setItem('state', JSON.stringify(this.state))
+        const zz=dataHub.find((y) => y.id ===this.state.id)
+        if(zz===undefined){
+          dataHub.push(this.state)
+        }else {
+          zz.counter=(this.state.counter)
+        }
+        
+
+        
+        window.localStorage.setItem('DataHub', JSON.stringify(dataHub));
+      }
+
+      
+      
+
+      
+      render(){
+        return(
+          <div className="item">
+            <div className="itemcard_up">
+                <img src={this.props.image} alt="item" />
+                <h1>{this.props.name}</h1>
+                <p>{this.props.desc}</p>
+              <div className="itemcard_down">
+                  <div className="total_price">
+                      <h2>$ {this.props.price}</h2>  
+                  </div>
+                  <div className="item_icons">
+                      <div>
+                          <i onClick={() => {this.DecreaseByOne(this.props.id)}}>-</i>  
+                      </div>
+                      <div className="one_item_counter">
+                          <h1>{this.state.counter}</h1>
+                      </div>
+                      <div>
+                          <i onClick={() => {this.IncreaseByOne(this.props.id)}}>+</i>
+                       </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+   
+    
+        );
+    }}
 
 export default Item;
